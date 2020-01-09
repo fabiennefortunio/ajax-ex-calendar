547c7096aaa306aa1bdc7846735a93ef9757792d
Fabienne <fortunio.fabienne@gmail.com>
2020-01-09 21:07:01 +0100

    structure

--- main.js
+++ main.js
@@ -16,7 +16,35 @@ function previousMonth(){
     }
 }
 
+function redraw(){
+    var calendar=$("#calendar");
+    var day;
 
+    var d=new Date(year,month,1);
+    var m=moment(d);
+    var daysInMonth=m.daysOfMonth();
+    var dayOfWeek=m.day();
+    var i;
+
+    var html="<table class='calendar'><tr>";
+
+    for(i=0;i<dayOfWeek;i++){
+        html=html+"<td><td>";
+    }
+
+    for(i=0;i<daysInMonth;i++){
+        html=html+"<td >"+i+"</td>";
+        m.add(1,'days');
+        dayOfWeek=m.day();
+        if(dayOfWeek==0){
+            html=html+"</tr><tr>";
+        }
+    }
+
+    html=htm
+    calendar.innerHTML=html;
+
+}
 
 
 $("document").ready(function() {
@@ -26,11 +54,12 @@ $("document").ready(function() {
     $("#btnPrevious").click(function(){
         previousMonth();
         window.alert(month);
-        //redraw();
+
+        redraw();
     });
     $("#btnNext").click(function(){
         nextMonth();
         window.alert(month);
-        //redraw();
+        redraw();
     });
 });
