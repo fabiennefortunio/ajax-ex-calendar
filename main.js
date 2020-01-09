080f5f291d01ca2ecc79b17dd17ea89b5ffd36a2
Fabienne <fortunio.fabienne@gmail.com>
2020-01-09 21:06:22 +0100

    structure

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
+var year=2018;
+var month;
+
+
+function nextMonth(){
+    month++;
+    if(month>11){
+        month=11;
+    }
+}
+
+function previousMonth(){
+    month--;
+    if(month<0){
+        month=0;
+    }
+}
+
+
+
+
 $("document").ready(function() {
 
-    
-})
+    month=0;
+
+    $("#btnPrevious").click(function(){
+        previousMonth();
+        window.alert(month);
+        //redraw();
+    });
+    $("#btnNext").click(function(){
+        nextMonth();
+        window.alert(month);
+        //redraw();
+    });
+});
